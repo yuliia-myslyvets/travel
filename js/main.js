332dded6d0a0cc3c358800be5ef2ac6ce869056d
yuliia-myslyvets <55326458+yuliia-myslyvets@users.noreply.github.com>
2022-09-06 12:09:49 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,24 @@
+(function () {
+    const header = document.querySelector('.header');
+    window.onscroll = () => {
+        if (window.pageYOffset > 50) {
+            header.classList.add('header_active');
+        } else {
+            header.classList.remove('header_active')
+        }
+    };
+}());
+
+// Burger handler
+(function () {
+    const burgerItem = document.querySelector('.burger');
+    const menu = document.querySelector('.header_nav');
+    const menuCloseItem = document.querySelector('.header_nav-close');
+    burgerItem.addEventListener('click', () => {
+        menu.classList.add('header_nav_active');
+    });
+    menuCloseItem.addEventListener('click', () => {
+        menu.classList.remove('header_nav_active');
+    });
+}());
+
